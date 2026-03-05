d0c96e01960f8d6f74647e4a4f1bfc259247ab82
RadikaTriezaAritonang <h1101241024@student.untan.ac.id>
2026-03-05 22:45:54 +0700

    Custom Validation Error

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,37 @@ import { openapi } from "@elysiajs/openapi";
 
 
 const app = new Elysia()
+  .get("/admin", () => ({ stats: 99 }), {
+    beforeHandle({ headers, set }) {
+      if (headers.authorization !== "Bearer 123") {
+        set.status = 401;
+        return { success: false, message: "Unauthorized" };
+      }
+    }
+  })
+
+  .post("/login", ({ body }) => ({ message: "Success", data: body }), {
+    body: t.Object({
+      email: t.String({ format: "email" }),
+      password: t.String({ minLength: 8 })
+    })
+  })
+
+  .onError(({ code, error, set }) => {
+    if (code === "VALIDATION") {
+      set.status = 400;
+      return {
+        success: false,
+        error: "Validation Error",
+        detail: error.message
+      };
+    }
+    
+    if (code === "NOT_FOUND") {
+      set.status = 404;
+      return { message: "Route not found" };
+    }
+  })
   .use(openapi())
 
 
@@ -61,6 +92,7 @@ const app = new Elysia()
     }
   )
 
+
   .get(
   "/admin",
   () => {
@@ -77,6 +109,7 @@ const app = new Elysia()
         };
       }
     }
+    
   }
 )
 
@@ -116,9 +149,8 @@ app.onError(({ code, error, set }) => {
      success: false,
      message: "Validation Error",
      detail: error.message
-   }
+   };
  }
-
 
  if (code === "NOT_FOUND") {
    set.status = 404
